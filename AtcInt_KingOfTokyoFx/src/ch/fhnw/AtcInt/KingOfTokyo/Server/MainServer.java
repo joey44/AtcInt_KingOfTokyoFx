acf9353862c146f032904acbeed932c2a4980925
EGJOE <EGJOE@PSTNEGJOE01.hs.coop.ch>
2015-12-17 08:23:09 +0100

    favicon Server

--- AtcInt_KingOfTokyoFx/src/ch/fhnw/AtcInt/KingOfTokyo/Server/MainServer.java
+++ AtcInt_KingOfTokyoFx/src/ch/fhnw/AtcInt/KingOfTokyo/Server/MainServer.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
@@ -23,6 +24,7 @@ public class MainServer extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("ServerGUI.fxml"));
 		primaryStage.setTitle("ServerGUI");
+		primaryStage.getIcons().add(new Image("/ch/fhnw/AtcInt/KingOfTokyo/Images/logo.png"));
 		primaryStage.setScene(new Scene(root, 340, 400));
 		primaryStage.show();
 
